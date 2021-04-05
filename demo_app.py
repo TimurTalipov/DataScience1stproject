2ff60bd635897e9db34505a4e26ddea4bb5aa292
project <timurtiger2014@gmail.com>
2021-04-05 15:30:32 +0300

    Data added saыs

--- demo_app.py
+++ demo_app.py
@@ -13,13 +13,13 @@ with st.echo(code_location='below'):
     """
     This is a test.
     """
-    GlobalTemp = pd.read_csv("/kaggle/input/climate-change-earth-surface-temperature-data/GlobalTemperatures.csv",
+    GlobalTemp = pd.read_csv("C:\Users\Xiaomi\PycharmProjects\DataScience1stproject/kaggle/input/climate-change-earth-surface-temperature-data/GlobalTemperatures.csv",
                              parse_dates=['dt'])
     GlobalTempCountry = pd.read_csv(
-        "/kaggle/input/climate-change-earth-surface-temperature-data/GlobalLandTemperaturesByCountry.csv",
+        "C:\Users\Xiaomi\PycharmProjects\DataScience1stproject/kaggle/input/climate-change-earth-surface-temperature-data/GlobalLandTemperaturesByCountry.csv",
         parse_dates=['dt'])
     GlobalTempState = pd.read_csv(
-        "/kaggle/input/climate-change-earth-surface-temperature-data/GlobalLandTemperaturesByState.csv",
+        "C:\Users\Xiaomi\PycharmProjects\DataScience1stproject/kaggle/input/climate-change-earth-surface-temperature-data/GlobalLandTemperaturesByState.csv",
         parse_dates=['dt'])
 
     GlobalTemp.head(5)
