5579c5ea7aa03121dc0416332bbfa6301e465dfa
project <timurtiger2014@gmail.com>
2021-04-05 13:35:23 +0300

    a

--- demo_app.py
+++ demo_app.py
@@ -1,14 +1,24 @@
 import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
+import streamlit.components.v1 as components
+from celluloid import Camera
+
+# Based on this recipe:
+# https://discuss.streamlit.io/t/matplotlib-animation-in-streamlit-through-html-js/5587
 
 with st.echo(code_location='below'):
     st.title("Hello, World!")
     """
-    This фффф is a test.
+    This is a test.
     """
     x = np.linspace(0, 10, 500)
     fig = plt.figure()
-    plt.plot(x, np.sin(x))
-    plt.ylim(-2, 2)
-    st.pyplot(fig)
+    camera = Camera(fig)
+    for i in range(10):
+        plt.plot(x, np.sin(x + i))
+        plt.ylim(-2, 2)
+        camera.snap()
+    animation = camera.animate()
+
+    components.html(animation.to_jshtml(), height=1000)
