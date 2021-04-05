2b50647205ef7fa1efc1f149392f60cd0f3fa709
project <timurtiger2014@gmail.com>
2021-04-05 16:27:49 +0300

    Data added saыs

--- demo_app.py
+++ demo_app.py
@@ -16,6 +16,6 @@ with st.echo(code_location='below'):
     GlobalTemp = pd.read_csv("https://gist.githubusercontent.com/TimurTalipov/68d7ca73f71349d63e685d7fb4c3b406/raw/35afc501060b9a53a79cad1ac8130df65748b3e4/GlobalTemperatures.csv")
     GlobalTempCountry = pd.read_csv("https://gist.githubusercontent.com/TimurTalipov/fb462f1045f4e227f2d3881cf864fb76/raw/eda82461e897b21a4cd729712293dee6e6994cef/GlobalLandTemperaturesByCountry.csv")
     GlobalTempState = pd.read_csv("https://gist.githubusercontent.com/TimurTalipov/1386e9213c52e49e93932efa2cb0cbea/raw/b7c2da931a3c23a41f44dcd380aab25464770ea8/GlobalLandTemperaturesByState.csv")
-    GlobalTempMajorCity = pr.read_csv("https://gist.githubusercontent.com/TimurTalipov/7b1b830dbdec4fe9e6a3816ab5a0e08d/raw/eac257bb757be6d1a67eed11198bf64f257ccfb0/GlobalLandTemperaturesByMajorCity.csv")
+    GlobalTempMajorCity = pd.read_csv("https://gist.githubusercontent.com/TimurTalipov/7b1b830dbdec4fe9e6a3816ab5a0e08d/raw/eac257bb757be6d1a67eed11198bf64f257ccfb0/GlobalLandTemperaturesByMajorCity.csv")
     st.write(GlobalTemp)
 
