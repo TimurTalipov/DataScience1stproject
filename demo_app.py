ef36360a9caa23417d7eddd0c7321c7964acb30e
project <timurtiger2014@gmail.com>
2021-04-08 18:04:23 +0300

    1st interactioin

--- demo_app.py
+++ demo_app.py
@@ -8,6 +8,7 @@ import plotly.express as px
 from celluloid import Camera
 from pandas import read_csv
 import cufflinks as cf
+import seaborn as sns
 
 
 # Based on this recipe:
@@ -15,33 +16,49 @@ import cufflinks as cf
 with st.echo(code_location='below'):
     st.title("Hello, World!")
     """
-    This is a test.
+    Добро пожаловать! Сегодня мы будем исследовать температуру и все что с ней связано
     """
-
-    cf.go_offline()
-    cf.set_config_file(offline=False, world_readable=True)
-    GlobalTemp = pd.read_csv("https://gist.githubusercontent.com/TimurTalipov/68d7ca73f71349d63e685d7fb4c3b406/raw/35afc501060b9a53a79cad1ac8130df65748b3e4/GlobalTemperatures.csv", parse_dates= ['dt'])
-    GlobalTempCountry = pd.read_csv("https://gist.githubusercontent.com/TimurTalipov/fb462f1045f4e227f2d3881cf864fb76/raw/eda82461e897b21a4cd729712293dee6e6994cef/GlobalLandTemperaturesByCountry.csv", parse_dates= ['dt'])
-    GlobalTempState = pd.read_csv("https://gist.githubusercontent.com/TimurTalipov/1386e9213c52e49e93932efa2cb0cbea/raw/b7c2da931a3c23a41f44dcd380aab25464770ea8/GlobalLandTemperaturesByState.csv", parse_dates= ['dt'])
-    GlobalTempMajorCity = pd.read_csv("https://gist.githubusercontent.com/TimurTalipov/7b1b830dbdec4fe9e6a3816ab5a0e08d/raw/eac257bb757be6d1a67eed11198bf64f257ccfb0/GlobalLandTemperaturesByMajorCity.csv", parse_dates= ['dt'])
-    st.write(GlobalTemp)
-
+    GlobalTemp = pd.read_csv(r"C:\Users\Xiaomi\PycharmProjects\DataScience1stproject/GlobalTemperatures.csv")
+    GlobalTempCountry = pd.read_csv(r"C:\Users\Xiaomi\PycharmProjects\DataScience1stproject/GlobalLandTemperaturesByCountry.csv")
+    GlobalTempState = pd.read_csv(r"C:\Users\Xiaomi\PycharmProjects\DataScience1stproject/GlobalLandTemperaturesByState.csv")
+    GlobalTempMajorCity = pd.read_csv(r"C:\Users\Xiaomi\PycharmProjects\DataScience1stproject/GlobalLandTemperaturesByMajorCity.csv")
+    GlobalTempCountry.dropna(axis=0, inplace=True)
+    GlobalTempMajorCity.dropna(axis=0, inplace=True)
     countries = np.unique(GlobalTempCountry['Country'])
+    majorcities = np.unique(GlobalTempMajorCity['City'])
+    st.write(countries)
+    st.write(majorcities)
     mean_temp = []
     for country in countries:
         mean_temp.append(GlobalTempCountry[GlobalTempCountry['Country'] == country]['AverageTemperature'].mean())
-    fig = go.Figure(data=go.Choropleth(
-        locations=countries,  # Spatial coordinates
-        z=mean_temp,  # Data to be color-coded
-        locationmode='country names',  # set of locations match entries in `locations`
-        text=countries,
-        colorscale='RdBu_r',
-        colorbar_title="Average Temp",
-    ))
 
-    fig.update_layout(
-        title_text='Average land temperature in countries',
+    mean_temp_bar, countries_bar = (list(x) for x in zip(*sorted(zip(mean_temp, countries),
+                                                                 reverse=True)))
+    sns.set(font_scale=0.7)
+    f, ax = plt.subplots(figsize=(5, 50))
+    colors_cw = sns.color_palette('magma', len(countries))
+    sns.barplot(mean_temp_bar, countries_bar, palette=colors_cw[::-1])
+    Text = ax.set(xlabel='Average temperature', title='Average  temperature in countries and lands')
+    st.pyplot(f)
 
-    )
+    mean_temp_cities = []
+    for city in majorcities:
+        mean_temp_cities.append(GlobalTempMajorCity[GlobalTempMajorCity['City'] == city]['AverageTemperature'].mean())
 
-    fig.show()
+    mean_temp_bar2, majorcities_bar = (list(x) for x in zip(*sorted(zip(mean_temp_cities, majorcities ),
+                                                                 reverse=True)))
+    sns.set(font_scale=1)
+    f2, ax = plt.subplots(figsize=(10, 30))
+    colors_cw = sns.color_palette('magma', len(majorcities))
+    sns.barplot(mean_temp_bar2, majorcities_bar, palette=colors_cw[::-1])
+    Text = ax.set(xlabel='Average temperature', title='Average temperature in cities')
+    st.pyplot(f2)
+
+    st.subheader("Из этих 2ух картинок мы можем сделать вывод об отрицательной корелляции средней температуры и уровня развития страны")
+    st.subheader("Кому холодно, тот больше старается, получается:)")
+    """
+    P.S В Дании тепло! Просто она долгое время владела Гренландией
+    """
+    st.subheader("И вообще, кто-то из вас мог себе представить, что Рим теплее Мадрида, и что Богота(средняя высота над уровнем моря 2640 м), теплее чем Сан Пауло?")
+
+
